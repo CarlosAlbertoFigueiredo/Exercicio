bf7822338f353fb64a3bb29b8e2af0d71747d85a
leaopensador <figueiredo.quanta@gmail.com>
2017-01-22 17:34:40 -0200

    Commit02

--- src/CadastroDeLivros.java
+++ src/CadastroDeLivros.java
@@ -10,20 +10,14 @@ public class CadastroDeLivros {
         livro.valor = 59.90;
         livro.isbn = "978-85-66250-46-6";
 
-        System.out.println(livro.nome);
-        System.out.println(livro.descricao);
-        System.out.println(livro.valor);
-        System.out.println(livro.isbn);
+        livro.mostrarDetalhes();
 
         Livro outrolivro = new Livro();
-        livro.nome = "Lógica de Programação";
-        livro.descricao = "Crie seus primeiros programas";
-        livro.valor = 59.90;
-        livro.isbn = "978-85-66250-22-0";
+        outrolivro.nome = "Lógica de Programação";
+        outrolivro.descricao = "Crie seus primeiros programas";
+        outrolivro.valor = 59.90;
+       outrolivro.isbn = "978-85-66250-22-0";
 
-        System.out.println(livro.nome);
-        System.out.println(livro.descricao);
-        System.out.println(livro.valor);
-        System.out.println(livro.isbn);
+        outrolivro.mostrarDetalhes();
     }
 }
--- src/Livro.java
+++ src/Livro.java
@@ -3,12 +3,26 @@
  */
 public class Livro {
 
+    //Declarações
+
     String nome;
     String descricao;
     double valor;
     String isbn;
 
+    //Métodos
 
+    public void mostrarDetalhes(){
+        String mensagem = "Mostrando detalhes do livro: ";
+        System.out.println(mensagem);
+        System.out.println("Nome:" + nome);
+        System.out.println("Descrição:" + descricao);
+        System.out.println("Valor:" + valor);
+        System.out.println("ISBN:" + isbn);
+        System.out.println("-----------------------");
+    }
+
+    //Getters e Setters
 
     public String getNome() {
         return nome;
@@ -41,6 +55,7 @@ public class Livro {
     public void setIsbn(String isbn) {
         this.isbn = isbn;
     }
+
 
 
 }
