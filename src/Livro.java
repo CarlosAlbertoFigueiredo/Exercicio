57963d2c982055174d6f206782bbbdc847e4776b
leaopensador <figueiredo.quanta@gmail.com>
2017-01-22 17:09:05 -0200

    Initial commit

---
+++ src/Livro.java
@@ -0,0 +1,46 @@
+/**
+ * Created by Usuario on 22/01/2017.
+ */
+public class Livro {
+
+    String nome;
+    String descricao;
+    double valor;
+    String isbn;
+
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+
+}
