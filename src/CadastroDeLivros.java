d62abcb683bd3fa3ec75d79282d1330d11a7c699
leaopensador <figueiredo.quanta@gmail.com>
2017-01-22 17:13:52 -0200

    Commit02

--- src/CadastroDeLivros.java
+++ src/CadastroDeLivros.java
@@ -14,5 +14,16 @@ public class CadastroDeLivros {
         System.out.println(livro.descricao);
         System.out.println(livro.valor);
         System.out.println(livro.isbn);
+
+        Livro outrolivro = new Livro();
+        livro.nome = "Lógica de Programação";
+        livro.descricao = "Crie seus primeiros programas";
+        livro.valor = 59.90;
+        livro.isbn = "978-85-66250-22-0";
+
+        System.out.println(livro.nome);
+        System.out.println(livro.descricao);
+        System.out.println(livro.valor);
+        System.out.println(livro.isbn);
     }
 }
